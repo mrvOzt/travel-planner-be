7477c821a5fdf35e798274ebe7b4b3bcb04abae1
mrvOzt <mervemr2601@gmail.com>
2024-07-03 16:48:32 +0300

    initial

---
+++ src/test/java/com/travel_planner_be/travel/TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel_planner_be.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
