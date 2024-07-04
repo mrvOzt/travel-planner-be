2ac91c0960f3d01bdc11cddb4ecb647d405cb8a6
OnurArslan0 <onursln11@gmail.com>
2024-07-04 12:08:17 +0300

    hata düzeltildi. TravelApplication.java anotasyon eklendi.

--- src/main/java/com/travel_planner_be/travel/TravelApplication.java
+++ src/main/java/com/travel_planner_be/travel/TravelApplication.java
@@ -2,7 +2,10 @@ package com.travel_planner_be.travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableMongoRepositories
 @SpringBootApplication
 public class TravelApplication {
 
