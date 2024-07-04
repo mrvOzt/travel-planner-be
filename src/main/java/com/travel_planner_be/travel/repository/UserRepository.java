67c1d81070e4d5bb54a392c83438eff2c27c1c4f
OnurArslan0 <onursln11@gmail.com>
2024-07-04 11:12:09 +0300

    user repository auth için findbyemail eklendi. application.yaml düzenlendi. db bağlantısı eklendi.

--- src/main/java/com/travel_planner_be/travel/repository/UserRepository.java
+++ src/main/java/com/travel_planner_be/travel/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.travel_planner_be.travel.repository;
 
-
+import com.travel_planner_be.travel.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface UserRepository {
+public interface UserRepository extends MongoRepository<User, String> {
 
-
+    Optional<User> findByEmail(String email);
 
 }
