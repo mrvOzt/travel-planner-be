4298c049e8b39e6144fb66edc6d5bc2bc514b9ae
OnurArslan0 <onursln11@gmail.com>
2024-07-11 12:17:15 +0300

    added new password creator

--- src/main/java/com/travel_planner_be/travel/controller/UserController.java
+++ src/main/java/com/travel_planner_be/travel/controller/UserController.java
@@ -4,12 +4,11 @@ package com.travel_planner_be.travel.controller;
 import com.travel_planner_be.travel.dto.LoginDTO;
 import com.travel_planner_be.travel.entity.User;
 import com.travel_planner_be.travel.service.UserService;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.context.Context;
 
 
 @RequestMapping("api/user")
@@ -29,5 +28,8 @@ public class UserController {
         return userService.saveUser(user);
     }
 
-
+    @PutMapping("/forgotPassword")
+    public ResponseEntity<?> forgotPassword(@RequestParam String email) throws MessagingException {
+        return userService.forgotPassword(email);
+    }
 }
--- src/main/java/com/travel_planner_be/travel/service/UserService.java
+++ src/main/java/com/travel_planner_be/travel/service/UserService.java
@@ -3,22 +3,30 @@ package com.travel_planner_be.travel.service;
 import com.travel_planner_be.travel.dto.LoginDTO;
 import com.travel_planner_be.travel.entity.User;
 import com.travel_planner_be.travel.repository.UserRepository;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 import java.util.Optional;
+import java.util.Random;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final TemplateEngine templateEngine;
+    private final JavaMailSender javaMailSender;
+
 
     public ResponseEntity<?> saveUser(User user) {
         if (user.getId() == null) {
@@ -64,9 +72,54 @@ public class UserService {
         return userRepository.findById(id);
     }
 
+    private Optional<User> getUserByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    private static String alphaNumericString(int len) {
+        String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Random rnd = new Random();
+
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+        }
+        return sb.toString();
+    }
 
 
 
+    public ResponseEntity<?> forgotPassword(String email) throws MessagingException {
+        Optional<User> tempUser = userRepository.findByEmail(email);
+        if (tempUser.isPresent()) {
+            String pass=sendEmailForgottenPassword(tempUser.get());
+            updatePassword(tempUser.get().getId(), pass);
+            return ResponseEntity.ok("We send your new password to your email.");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+    }
 
+    private String sendEmailForgottenPassword(User tempUser) throws MessagingException {
 
+        String freshPassword = alphaNumericString(8);
+        Context context = new Context();
+        context.setVariable("email", tempUser.getEmail());
+        context.setVariable("newPassword", freshPassword);
+        String htmlBody = templateEngine.process("password", context);
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(tempUser.getEmail());
+        helper.setSubject("Here is your new password");
+        helper.setText(htmlBody, true);
+        javaMailSender.send(message);
+        return freshPassword;
+    }
+
+    private void updatePassword(String id, String newPassword) {
+        User user = userRepository.findById(id).get();
+        user.setPassword(newPassword);
+        userRepository.save(user);
+    }
 }
